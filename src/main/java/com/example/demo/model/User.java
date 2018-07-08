b20619f2077b52dd059e1c176ab8214c1f5a9112
qipeixuan <qipeixuan22@163.com>
2018-07-08 22:56:18 +0800

    Add Some File

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,18 @@
+package com.example.demo.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private long uid;   //用户的ID
+    private String username;    //用户名
+}
+
+
+/*
+Create Table User(
+    `uid` INT NOT NULL AUTO_INCREMENT,
+    `username` TEXT NOT NULL,
+    PRIMARY KEY(`uid`)
+);
+ */
