2b0ab40a33f3a2da10e6c30fcb50466233eab952
fishifishfish <beautifulzz666@126.com>
2018-07-11 21:44:34 +0800

    fix

--- src/main/java/com/example/demo/Service/Impl/CommentServiceImpl.java
+++ src/main/java/com/example/demo/Service/Impl/CommentServiceImpl.java
@@ -1,4 +1,46 @@
 package com.example.demo.Service.Impl;
 
-public class CommentServiceImpl {
+import com.example.demo.Dao.CommentDao;
+import com.example.demo.Dao.ProposalDao;
+import com.example.demo.Model.Comment;
+import com.example.demo.Service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommentServiceImpl implements CommentService {
+
+    @Autowired
+    CommentDao commentDao;
+
+    @Autowired
+    ProposalDao proposalDao;
+
+    @Override
+    public List<Comment> getCommentsByPid(int pid) {
+        return commentDao.getCommentsByPid(pid);
+    }
+
+    @Override
+    public int comment(int pid, int uid, String content, String opinion) {
+        Comment comment = commentDao.getCommentByPidAndUid(pid, uid);
+        if (comment == null) {
+            comment = new Comment(pid, uid, content);
+            int r = commentDao.insert(comment);
+            if (r > 0) {
+                if("support".equals(opinion)){
+                    proposalDao.support(pid);
+                }else{
+                    proposalDao.reject(pid);
+                }
+                return comment.getCid();
+            } else {
+                return -1;
+            }
+        } else {
+            return 0;
+        }
+    }
 }
--- src/main/java/com/example/demo/Service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/Service/Impl/UserServiceImpl.java
@@ -1,4 +1,46 @@
 package com.example.demo.Service.Impl;
 
-public class UserServiceImpl {
+import com.example.demo.Model.User;
+import com.example.demo.Service.UserService;
+import com.example.demo.Util.MultiResult;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Override
+    public MultiResult<User> login(String username, String password) {
+        return null;
+    }
+
+    @Override
+    public String register(User user) {
+        return null;
+    }
+
+    @Override
+    public void changeStatus(int uid, int status) {
+
+    }
+
+    @Override
+    public User getUserByUid(int uid) {
+        return null;
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public List<User> getUnVerifyUsers() {
+        return null;
+    }
 }
